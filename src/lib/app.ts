f918b68faf7d455e69d2d519130eecf683e3ccbf
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-02-03 22:58:14 +0600

    feat(app):using globalError middleware in app

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -2,10 +2,12 @@ import { toNodeHandler } from "better-auth/node";
 import express, { Application } from "express"
 import cors from "cors"
 import { auth } from "./auth.js";
+import globalErrorHandler from "../middlewares/globalErrorHandler.js";
 const app:Application = express()
-
+app.use(globalErrorHandler);
 app.use(express.json())
 app.use(cors({
+  
   origin:process.env.APP_URL ||"http://localhost:5000",
     credentials:true
 }
