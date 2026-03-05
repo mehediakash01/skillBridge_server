68a75ee9db438855847eb39c58997a1ca495764e
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-03-06 01:17:02 +0600

    fix(cors): update CORS origin URL to correct environment variable

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -17,7 +17,7 @@ app.use(globalErrorHandler);
 app.use(express.json())
 app.use(cors({
   
-  origin:process.env.APP_URL ||"https://skill-bridge-client-ex6c.vercel.app",
+  origin:process.env.APP_URL ||"https://skill-bridge-client-1h8j.vercel.app",
     credentials:true
 }
   
